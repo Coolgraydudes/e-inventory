266e60319d6c916055f25cb5472f859c00c52292
Coolgraydudes <144977851+Coolgraydudes@users.noreply.github.com>
2026-04-27 13:44:45 +0700

    error idk

--- src/store.js
+++ src/store.js
@@ -1,8 +1,12 @@
-import { configureStore } from "@reduxjs/toolkit";
-import inventoryReducer from "./inventorySlice";
+// import { configureStore } from "@reduxjs/toolkit";
+// import inventoryReducer from "./inventorySlice";
 
-export const store = configureStore({
-  reducer: {
-    inventory: inventoryReducer,
-  },
-});
+// export const store = configureStore({
+//   reducer: {
+//     inventory: inventoryReducer,
+//   },
+// });
+
+
+
+// di sini error tad ana nggk tau 
